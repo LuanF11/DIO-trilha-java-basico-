f9243c734a8ea67aa6f7f7d393edee7fbb8eb47b
LuanF11 <luaninfo09@gmail.com>
2024-04-29 12:50:19 -0300

    feat(ContaTerminal, Conta) - Criando a demonstracao dos dados

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        scanner.nextLine();
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+
+        Conta conta = new Conta(numero, agencia, nomeCliente, saldo);
+        System.out.println(conta);
+    }
+}
