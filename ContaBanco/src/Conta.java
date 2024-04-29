315de26273cff64198ed0e998e05af1ecf4578c8
LuanF11 <luaninfo09@gmail.com>
2024-04-29 12:49:13 -0300

    feat(ContaTerminal, Conta) - Criando a demonstracao dos dados

---
+++ ContaBanco/src/Conta.java
@@ -0,0 +1,19 @@
+public class Conta {
+    int numero;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+    public Conta(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
+                nomeCliente, agencia, numero, saldo);
+    }
+}
